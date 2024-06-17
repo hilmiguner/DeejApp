80151787fe85340ae7b27befd03455f043941b8c
hilmiguner <hilmi.guner@hotmail.com>
2024-06-17 22:22:15 +0300

    Audio management of a process feature has been added to app. Altering multiple audio of processes also works.

--- MainScreen.Designer.cs
+++ MainScreen.Designer.cs
@@ -28,16 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainScreen));
             this.label_sesSeviyesi = new System.Windows.Forms.Label();
             this.label_masterVolume = new System.Windows.Forms.Label();
             this.trackBar_masterVolume = new System.Windows.Forms.TrackBar();
+            this.comboBox_processOne = new System.Windows.Forms.ComboBox();
+            this.pictureBox_processOne = new System.Windows.Forms.PictureBox();
+            this.trackBar_processOne = new System.Windows.Forms.TrackBar();
+            this.label_processOneVolumeLevel = new System.Windows.Forms.Label();
+            this.label_sesSeviyesiProcessOne = new System.Windows.Forms.Label();
+            this.label_applicationOne = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox_masterVolume = new System.Windows.Forms.PictureBox();
+            this.label_applicationTwo = new System.Windows.Forms.Label();
+            this.label_processTwoVolumeLevel = new System.Windows.Forms.Label();
+            this.label_sesSeviyesiProcessTwo = new System.Windows.Forms.Label();
+            this.trackBar_processTwo = new System.Windows.Forms.TrackBar();
+            this.pictureBox_processTwo = new System.Windows.Forms.PictureBox();
+            this.comboBox_processTwo = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar_masterVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_processOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_processOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_masterVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_processTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_processTwo)).BeginInit();
             this.SuspendLayout();
             // 
             // label_sesSeviyesi
             // 
             this.label_sesSeviyesi.AutoSize = true;
-            this.label_sesSeviyesi.Location = new System.Drawing.Point(12, 9);
+            this.label_sesSeviyesi.Location = new System.Drawing.Point(23, 120);
             this.label_sesSeviyesi.Name = "label_sesSeviyesi";
             this.label_sesSeviyesi.Size = new System.Drawing.Size(73, 13);
             this.label_sesSeviyesi.TabIndex = 0;
@@ -46,30 +66,180 @@
             // label_masterVolume
             // 
             this.label_masterVolume.AutoSize = true;
-            this.label_masterVolume.Location = new System.Drawing.Point(81, 9);
+            this.label_masterVolume.Location = new System.Drawing.Point(92, 120);
             this.label_masterVolume.Name = "label_masterVolume";
-            this.label_masterVolume.Size = new System.Drawing.Size(0, 13);
+            this.label_masterVolume.Size = new System.Drawing.Size(60, 13);
             this.label_masterVolume.TabIndex = 1;
+            this.label_masterVolume.Text = "master_ses";
             // 
             // trackBar_masterVolume
             // 
-            this.trackBar_masterVolume.Location = new System.Drawing.Point(12, 36);
+            this.trackBar_masterVolume.Location = new System.Drawing.Point(26, 136);
+            this.trackBar_masterVolume.Maximum = 100;
             this.trackBar_masterVolume.Name = "trackBar_masterVolume";
-            this.trackBar_masterVolume.Size = new System.Drawing.Size(104, 45);
+            this.trackBar_masterVolume.Size = new System.Drawing.Size(112, 45);
             this.trackBar_masterVolume.TabIndex = 2;
             this.trackBar_masterVolume.Scroll += new System.EventHandler(this.trackBar_masterVolume_Scroll);
+            // 
+            // comboBox_processOne
+            // 
+            this.comboBox_processOne.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_processOne.FormattingEnabled = true;
+            this.comboBox_processOne.Location = new System.Drawing.Point(174, 92);
+            this.comboBox_processOne.Name = "comboBox_processOne";
+            this.comboBox_processOne.Size = new System.Drawing.Size(112, 21);
+            this.comboBox_processOne.TabIndex = 3;
+            this.comboBox_processOne.SelectedIndexChanged += new System.EventHandler(this.comboBox_processOne_SelectedIndexChanged);
+            // 
+            // pictureBox_processOne
+            // 
+            this.pictureBox_processOne.Location = new System.Drawing.Point(204, 36);
+            this.pictureBox_processOne.Name = "pictureBox_processOne";
+            this.pictureBox_processOne.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox_processOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_processOne.TabIndex = 4;
+            this.pictureBox_processOne.TabStop = false;
+            // 
+            // trackBar_processOne
+            // 
+            this.trackBar_processOne.Location = new System.Drawing.Point(174, 136);
+            this.trackBar_processOne.Maximum = 100;
+            this.trackBar_processOne.Name = "trackBar_processOne";
+            this.trackBar_processOne.Size = new System.Drawing.Size(112, 45);
+            this.trackBar_processOne.TabIndex = 5;
+            this.trackBar_processOne.Scroll += new System.EventHandler(this.trackBar_processOne_Scroll);
+            // 
+            // label_processOneVolumeLevel
+            // 
+            this.label_processOneVolumeLevel.AutoSize = true;
+            this.label_processOneVolumeLevel.Location = new System.Drawing.Point(240, 120);
+            this.label_processOneVolumeLevel.Name = "label_processOneVolumeLevel";
+            this.label_processOneVolumeLevel.Size = new System.Drawing.Size(64, 13);
+            this.label_processOneVolumeLevel.TabIndex = 7;
+            this.label_processOneVolumeLevel.Text = "session_ses";
+            // 
+            // label_sesSeviyesiProcessOne
+            // 
+            this.label_sesSeviyesiProcessOne.AutoSize = true;
+            this.label_sesSeviyesiProcessOne.Location = new System.Drawing.Point(171, 120);
+            this.label_sesSeviyesiProcessOne.Name = "label_sesSeviyesiProcessOne";
+            this.label_sesSeviyesiProcessOne.Size = new System.Drawing.Size(73, 13);
+            this.label_sesSeviyesiProcessOne.TabIndex = 6;
+            this.label_sesSeviyesiProcessOne.Text = "Ses Seviyesi: ";
+            // 
+            // label_applicationOne
+            // 
+            this.label_applicationOne.AutoSize = true;
+            this.label_applicationOne.Location = new System.Drawing.Point(196, 9);
+            this.label_applicationOne.Name = "label_applicationOne";
+            this.label_applicationOne.Size = new System.Drawing.Size(68, 13);
+            this.label_applicationOne.TabIndex = 8;
+            this.label_applicationOne.Text = "Application 1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Master Audio";
+            // 
+            // pictureBox_masterVolume
+            // 
+            this.pictureBox_masterVolume.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_masterVolume.Image")));
+            this.pictureBox_masterVolume.Location = new System.Drawing.Point(57, 36);
+            this.pictureBox_masterVolume.Name = "pictureBox_masterVolume";
+            this.pictureBox_masterVolume.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox_masterVolume.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_masterVolume.TabIndex = 10;
+            this.pictureBox_masterVolume.TabStop = false;
+            // 
+            // label_applicationTwo
+            // 
+            this.label_applicationTwo.AutoSize = true;
+            this.label_applicationTwo.Location = new System.Drawing.Point(344, 9);
+            this.label_applicationTwo.Name = "label_applicationTwo";
+            this.label_applicationTwo.Size = new System.Drawing.Size(68, 13);
+            this.label_applicationTwo.TabIndex = 16;
+            this.label_applicationTwo.Text = "Application 2";
+            // 
+            // label_processTwoVolumeLevel
+            // 
+            this.label_processTwoVolumeLevel.AutoSize = true;
+            this.label_processTwoVolumeLevel.Location = new System.Drawing.Point(388, 120);
+            this.label_processTwoVolumeLevel.Name = "label_processTwoVolumeLevel";
+            this.label_processTwoVolumeLevel.Size = new System.Drawing.Size(64, 13);
+            this.label_processTwoVolumeLevel.TabIndex = 15;
+            this.label_processTwoVolumeLevel.Text = "session_ses";
+            // 
+            // label_sesSeviyesiProcessTwo
+            // 
+            this.label_sesSeviyesiProcessTwo.AutoSize = true;
+            this.label_sesSeviyesiProcessTwo.Location = new System.Drawing.Point(319, 120);
+            this.label_sesSeviyesiProcessTwo.Name = "label_sesSeviyesiProcessTwo";
+            this.label_sesSeviyesiProcessTwo.Size = new System.Drawing.Size(73, 13);
+            this.label_sesSeviyesiProcessTwo.TabIndex = 14;
+            this.label_sesSeviyesiProcessTwo.Text = "Ses Seviyesi: ";
+            // 
+            // trackBar_processTwo
+            // 
+            this.trackBar_processTwo.Location = new System.Drawing.Point(322, 136);
+            this.trackBar_processTwo.Maximum = 100;
+            this.trackBar_processTwo.Name = "trackBar_processTwo";
+            this.trackBar_processTwo.Size = new System.Drawing.Size(112, 45);
+            this.trackBar_processTwo.TabIndex = 13;
+            this.trackBar_processTwo.Scroll += new System.EventHandler(this.trackBar_processTwo_Scroll);
+            // 
+            // pictureBox_processTwo
+            // 
+            this.pictureBox_processTwo.Location = new System.Drawing.Point(352, 36);
+            this.pictureBox_processTwo.Name = "pictureBox_processTwo";
+            this.pictureBox_processTwo.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox_processTwo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_processTwo.TabIndex = 12;
+            this.pictureBox_processTwo.TabStop = false;
+            // 
+            // comboBox_processTwo
+            // 
+            this.comboBox_processTwo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_processTwo.FormattingEnabled = true;
+            this.comboBox_processTwo.Location = new System.Drawing.Point(322, 92);
+            this.comboBox_processTwo.Name = "comboBox_processTwo";
+            this.comboBox_processTwo.Size = new System.Drawing.Size(112, 21);
+            this.comboBox_processTwo.TabIndex = 11;
+            this.comboBox_processTwo.SelectedIndexChanged += new System.EventHandler(this.comboBox_processTwo_SelectedIndexChanged);
             // 
             // MainScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_applicationTwo);
+            this.Controls.Add(this.label_processTwoVolumeLevel);
+            this.Controls.Add(this.label_sesSeviyesiProcessTwo);
+            this.Controls.Add(this.trackBar_processTwo);
+            this.Controls.Add(this.pictureBox_processTwo);
+            this.Controls.Add(this.comboBox_processTwo);
+            this.Controls.Add(this.pictureBox_masterVolume);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_applicationOne);
+            this.Controls.Add(this.label_processOneVolumeLevel);
+            this.Controls.Add(this.label_sesSeviyesiProcessOne);
+            this.Controls.Add(this.trackBar_processOne);
+            this.Controls.Add(this.pictureBox_processOne);
+            this.Controls.Add(this.comboBox_processOne);
             this.Controls.Add(this.trackBar_masterVolume);
             this.Controls.Add(this.label_masterVolume);
             this.Controls.Add(this.label_sesSeviyesi);
             this.Name = "MainScreen";
             this.Text = "Deej App";
             ((System.ComponentModel.ISupportInitialize)(this.trackBar_masterVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_processOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_processOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_masterVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_processTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_processTwo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,5 +250,19 @@
         private System.Windows.Forms.Label label_sesSeviyesi;
         private System.Windows.Forms.Label label_masterVolume;
         private System.Windows.Forms.TrackBar trackBar_masterVolume;
+        private System.Windows.Forms.ComboBox comboBox_processOne;
+        private System.Windows.Forms.PictureBox pictureBox_processOne;
+        private System.Windows.Forms.TrackBar trackBar_processOne;
+        private System.Windows.Forms.Label label_processOneVolumeLevel;
+        private System.Windows.Forms.Label label_sesSeviyesiProcessOne;
+        private System.Windows.Forms.Label label_applicationOne;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox_masterVolume;
+        private System.Windows.Forms.Label label_applicationTwo;
+        private System.Windows.Forms.Label label_processTwoVolumeLevel;
+        private System.Windows.Forms.Label label_sesSeviyesiProcessTwo;
+        private System.Windows.Forms.TrackBar trackBar_processTwo;
+        private System.Windows.Forms.PictureBox pictureBox_processTwo;
+        private System.Windows.Forms.ComboBox comboBox_processTwo;
     }
 }
--- MainScreen.cs
+++ MainScreen.cs
@@ -7,7 +7,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
+using static System.Collections.Specialized.BitVector32;
 
 namespace DeejApp
 {
@@ -15,18 +17,26 @@ namespace DeejApp
     {
         private MMDeviceEnumerator deviceEnumerator;
         private MMDevice audioDevice;
+        private SessionItem[] currentSessions = new SessionItem[2];
+        private ComboBox[] comboBoxes = new ComboBox[2];
+        private TrackBar[] trackBars = new TrackBar[2];
         public MainScreen()
         {
             InitializeComponent();
             deviceEnumerator = new MMDeviceEnumerator();
             audioDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-            trackBar_masterVolume.Minimum = 0;
-            trackBar_masterVolume.Maximum = 100;
+            InitializeMasterVolumeSystem();
+            InitializeComboBoxes();
+            InitializeTrackBars();
+            InitializeSessions();
+        }
+        
+        private void InitializeMasterVolumeSystem()
+        {
             trackBar_masterVolume.Value = (int)(audioDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
             label_masterVolume.Text = trackBar_masterVolume.Value.ToString();
             trackBar_masterVolume.Scroll += trackBar_masterVolume_Scroll;
         }
-
         private void trackBar_masterVolume_Scroll(object sender, EventArgs e)
         {
             float volume = trackBar_masterVolume.Value / 100.0f;
@@ -40,6 +50,138 @@ namespace DeejApp
             }
             audioDevice.AudioEndpointVolume.MasterVolumeLevelScalar = volume;
             label_masterVolume.Text = trackBar_masterVolume.Value.ToString();
+        }
+
+        private class SessionItem
+        {
+            public string name { get; set; }
+            public AudioSessionControl controller { get; set; }
+            public Icon icon { get; set; }
+        }
+
+        private List<SessionItem> getSessions()
+        {
+            List<SessionItem> sessionList = new List<SessionItem>();
+
+            SessionCollection allSessions = this.audioDevice.AudioSessionManager.Sessions;
+            for (int i = 0; i < allSessions.Count; i++)
+            {
+                AudioSessionControl sessionController = allSessions[i];
+                var process = Process.GetProcessById((int)sessionController.GetProcessID);
+                Icon icon = null;
+                String name = null;
+                if (sessionController.GetProcessID != 0)
+                {
+                    try
+                    {
+                        name = process.ProcessName;
+                        icon = Icon.ExtractAssociatedIcon(process.MainModule.FileName);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                    sessionList.Add(new SessionItem { controller = sessionController, icon = icon, name = name });
+                }
+            }
+            return sessionList;
+        }
+
+        private void InitializeComboBoxes()
+        {
+            comboBoxes[0] = this.comboBox_processOne;
+            comboBoxes[1] = this.comboBox_processTwo;
+        }
+
+        private void InitializeTrackBars()
+        {
+            trackBars[0] = this.trackBar_processOne;
+            trackBars[1] = this.trackBar_processTwo;
+        }
+
+        private void InitializeSessions()
+        {
+            foreach (ComboBox comboBox in comboBoxes)
+            {
+                List<SessionItem> sessionList = getSessions();
+                comboBox.DataSource = sessionList;
+                comboBox.DisplayMember = "name";
+            }
+        }
+
+        private void updateTrackBarsByName(int parentIndex, String name, float volume)
+        {
+            int count = 0;
+            foreach (var session in currentSessions)
+            {
+                if (session.name == name && count != parentIndex) 
+                {
+                   trackBars[count].Value = (int)(this.currentSessions[count].controller.SimpleAudioVolume.Volume * 100);
+                }
+                count++;
+            }
+        }
+
+        private void updateLabels()
+        {
+            this.label_processOneVolumeLevel.Text = this.trackBar_processOne.Value.ToString();
+            this.label_processTwoVolumeLevel.Text = this.trackBar_processTwo.Value.ToString();
+        }
+
+        private void comboBox_processOne_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.currentSessions[0] = this.comboBox_processOne.SelectedItem as SessionItem;
+            this.pictureBox_processOne.Image = this.currentSessions[0].icon.ToBitmap();
+            this.trackBar_processOne.Value = (int)(this.currentSessions[0].controller.SimpleAudioVolume.Volume * 100);
+            label_processOneVolumeLevel.Text = trackBar_processOne.Value.ToString();
+        }
+
+        private void trackBar_processOne_Scroll(object sender, EventArgs e)
+        {
+            float volume = trackBar_processOne.Value / 100.0f;
+
+            // Mute check BEGIN
+            if (volume == 0.0f)
+            {
+                this.currentSessions[0].controller.SimpleAudioVolume.Mute = true;
+            }
+            else
+            {
+                if (this.currentSessions[0].controller.SimpleAudioVolume.Mute == true) this.currentSessions[0].controller.SimpleAudioVolume.Mute = false;
+            }
+            // Mute check END
+
+            this.currentSessions[0].controller.SimpleAudioVolume.Volume = volume;
+            updateTrackBarsByName(0, this.currentSessions[0].name, volume);
+            updateLabels();
+        }
+
+        private void comboBox_processTwo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.currentSessions[1] = this.comboBox_processTwo.SelectedItem as SessionItem;
+            this.pictureBox_processTwo.Image = this.currentSessions[1].icon.ToBitmap();
+            this.trackBar_processTwo.Value = (int)(this.currentSessions[1].controller.SimpleAudioVolume.Volume * 100);
+            label_processTwoVolumeLevel.Text = trackBar_processTwo.Value.ToString();
+        }
+
+        private void trackBar_processTwo_Scroll(object sender, EventArgs e)
+        {
+            float volume = trackBar_processTwo.Value / 100.0f;
+
+            // Mute check BEGIN
+            if (volume == 0.0f)
+            {
+                this.currentSessions[1].controller.SimpleAudioVolume.Mute = true;
+            }
+            else
+            {
+                if (this.currentSessions[1].controller.SimpleAudioVolume.Mute == true) this.currentSessions[1].controller.SimpleAudioVolume.Mute = false;
+            }
+            // Mute check END
+
+            this.currentSessions[1].controller.SimpleAudioVolume.Volume = volume;
+            updateTrackBarsByName(1, this.currentSessions[1].name, volume);
+            updateLabels();
         }
     }
 }
