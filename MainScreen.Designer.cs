071ecada23ebde2a2a1ea788a37c0e4f56ec3383
hilmiguner <hilmi.guner@hotmail.com>
2024-06-17 15:44:24 +0300

    Initial commit

---
+++ MainScreen.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace DeejApp
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_sesSeviyesi = new System.Windows.Forms.Label();
+            this.label_masterVolume = new System.Windows.Forms.Label();
+            this.trackBar_masterVolume = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_masterVolume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_sesSeviyesi
+            // 
+            this.label_sesSeviyesi.AutoSize = true;
+            this.label_sesSeviyesi.Location = new System.Drawing.Point(12, 9);
+            this.label_sesSeviyesi.Name = "label_sesSeviyesi";
+            this.label_sesSeviyesi.Size = new System.Drawing.Size(73, 13);
+            this.label_sesSeviyesi.TabIndex = 0;
+            this.label_sesSeviyesi.Text = "Ses Seviyesi: ";
+            // 
+            // label_masterVolume
+            // 
+            this.label_masterVolume.AutoSize = true;
+            this.label_masterVolume.Location = new System.Drawing.Point(81, 9);
+            this.label_masterVolume.Name = "label_masterVolume";
+            this.label_masterVolume.Size = new System.Drawing.Size(0, 13);
+            this.label_masterVolume.TabIndex = 1;
+            // 
+            // trackBar_masterVolume
+            // 
+            this.trackBar_masterVolume.Location = new System.Drawing.Point(12, 36);
+            this.trackBar_masterVolume.Name = "trackBar_masterVolume";
+            this.trackBar_masterVolume.Size = new System.Drawing.Size(104, 45);
+            this.trackBar_masterVolume.TabIndex = 2;
+            this.trackBar_masterVolume.Scroll += new System.EventHandler(this.trackBar_masterVolume_Scroll);
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar_masterVolume);
+            this.Controls.Add(this.label_masterVolume);
+            this.Controls.Add(this.label_sesSeviyesi);
+            this.Name = "MainScreen";
+            this.Text = "Deej App";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_masterVolume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_sesSeviyesi;
+        private System.Windows.Forms.Label label_masterVolume;
+        private System.Windows.Forms.TrackBar trackBar_masterVolume;
+    }
+}
---
+++ MainScreen.cs
@@ -0,0 +1,37 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeejApp
+{
+    public partial class MainScreen : Form
+    {
+        private MMDeviceEnumerator deviceEnumerator;
+        private MMDevice audioDevice;
+        public MainScreen()
+        {
+            InitializeComponent();
+            deviceEnumerator = new MMDeviceEnumerator();
+            audioDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            trackBar_masterVolume.Minimum = 0;
+            trackBar_masterVolume.Maximum = 100;
+            trackBar_masterVolume.Value = (int)(audioDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+            label_masterVolume.Text = trackBar_masterVolume.Value.ToString();
+            trackBar_masterVolume.Scroll += trackBar_masterVolume_Scroll;
+        }
+
+        private void trackBar_masterVolume_Scroll(object sender, EventArgs e)
+        {
+            float volume = trackBar_masterVolume.Value / 100.0f;
+            audioDevice.AudioEndpointVolume.MasterVolumeLevelScalar = volume;
+            label_masterVolume.Text = trackBar_masterVolume.Value.ToString();
+        }
+    }
+}
